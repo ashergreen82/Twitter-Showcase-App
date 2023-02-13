331fca436c1be1ee660190ef59fbccacaa91bc07
Ashergreen82 <greena@rogers.com>
2023-02-12 19:32:16 -0500

    Created the background

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+// import Main from './Main';
+import background from "./images/background.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div style={{ backgroundImage: `url(${background})`, backgroundSize: "cover", backgroundRepeat: "no-repeat", height: "100vh" }} >
+        <div className="container">
+          {/* <Main /> */}
+        </div>
+      </div>
+    </>
   );
 }
 
