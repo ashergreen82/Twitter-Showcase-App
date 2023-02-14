4c84d0bc791b2ea3287a50897e5f4fdb465533ff
Ashergreen82 <greena@rogers.com>
2023-02-14 06:02:02 -0500

    Changed the "class" to "classname"

--- src/Main.js
+++ src/Main.js
@@ -8,27 +8,27 @@ function mainNavBar() {
 
     return (
         <>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <a class="navbar-brand" href="#"></a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <a className="navbar-brand" href="#"></a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse w-100" id="navbarNav">
-                    <ul class="navbar-nav d-flex flex-row w-100 justify-content-between border border-warning">
-                        <li class="nav-item col-2">
-                            <a class="nav-link" href="#">Main</a>
+                <div className="collapse navbar-collapse w-100" id="navbarNav">
+                    <ul className="navbar-nav d-flex flex-row w-100 justify-content-between border border-warning">
+                        <li className="nav-item col-2">
+                            <a className="nav-link" href="#">Main</a>
                         </li>
-                        {/* <div class="vr mx-5"></div> */}
-                        <li class="nav-item col-8">
-                            <form class="d-flex flex-row flex-nowrap m-0 p-0">
-                                <input class="form-control col-8" type="search" placeholder="Search" aria-label="Search" />
-                                <button class="btn col-4 btn-outline-success" type="submit">Search</button>
+                        {/* <div className="vr mx-5"></div> */}
+                        <li className="nav-item col-8">
+                            <form className="d-flex flex-row flex-nowrap m-0 p-0">
+                                <input className="form-control col-8" type="search" placeholder="Search" aria-label="Search" />
+                                <button className="btn col-4 btn-outline-success" type="submit">Search</button>
                             </form>
-                            {/* <a class="nav-link" href="#">Search</a> */}
+                            {/* <a className="nav-link" href="#">Search</a> */}
                         </li>
-                        {/* <div class="vr mx-5"></div> */}
-                        <li class="nav-item col-2">
-                            <a class="nav-link" href="#">Random Search</a>
+                        {/* <div className="vr mx-5"></div> */}
+                        <li className="nav-item col-2">
+                            <a className="nav-link" href="#">Random Search</a>
                         </li>
                     </ul>
                 </div>
