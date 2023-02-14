a8fdf6ad5664a770fb96cece1a352375471808c5
Ashergreen82 <greena@rogers.com>
2023-02-13 20:33:27 -0500

    Trying to get nav bar to work

--- src/Main.js
+++ src/Main.js
@@ -1,23 +1,26 @@
-import React from 'react'
-import { useEffect, useState } from 'react';
+import React from 'react';
+// import { useEffect, useState } from 'react';
 // import axios from 'axios';
 
 import { Navbar, Nav, Container } from 'react-bootstrap';
 
-export default function StarWars() {
+function mainNavBar() {
 
     return (
         <Navbar bg="light" expand="lg">
             <Container>
-                <Navbar.Brand href="#home">My Menu Bar</Navbar.Brand>
+                <Navbar.Brand href="#home">Twitter Showcase App</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
                         <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
+                        <Nav.Link href="#home">Search</Nav.Link>
+                        <Nav.Link href="#link">Random Search</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
     )
 }
+
+export default mainNavBar;
