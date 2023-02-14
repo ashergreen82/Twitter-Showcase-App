701033052a7154949d61f536b1786fcdbbd4864a
Ashergreen82 <greena@rogers.com>
2023-02-13 22:25:37 -0500

    Still trying to get navbar to work

--- src/Main.js
+++ src/Main.js
@@ -13,15 +13,21 @@ function mainNavBar() {
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
+                <div class="collapse navbar-collapse w-100" id="navbarNav">
+                    <ul class="navbar-nav d-flex flex-row w-100 justify-content-between border border-warning">
+                        <li class="nav-item col-2">
                             <a class="nav-link" href="#">Main</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Search</a>
+                        {/* <div class="vr mx-5"></div> */}
+                        <li class="nav-item col-8">
+                            <form class="d-flex flex-row flex-nowrap m-0 p-0">
+                                <input class="form-control col-8" type="search" placeholder="Search" aria-label="Search" />
+                                <button class="btn col-4 btn-outline-success" type="submit">Search</button>
+                            </form>
+                            {/* <a class="nav-link" href="#">Search</a> */}
                         </li>
-                        <li class="nav-item">
+                        {/* <div class="vr mx-5"></div> */}
+                        <li class="nav-item col-2">
                             <a class="nav-link" href="#">Random Search</a>
                         </li>
                     </ul>
