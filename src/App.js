e6a46b1226d74c50b23406e3ab2117919b5198cb
Ashergreen82 <greena@rogers.com>
2023-02-14 07:06:27 -0500

    managed to center background on the screen

--- src/App.js
+++ src/App.js
@@ -6,7 +6,15 @@ import background from "./images/alexander-shatov-SXfwXS0jWNg-unsplash.jpg";
 function App() {
   return (
     <>
-      <div style={{ backgroundImage: `url(${background})`, backgroundSize: "cover", backgroundRepeat: "no-repeat", height: "100vh" }} >
+      <div style={{
+        backgroundImage: `url(${background})`,
+        backgroundSize: "cover",
+        // backgroundSize: "contain",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "Center",
+        height: "100vh",
+        width: "100%"
+      }} >
         <div className="container">
           <Main />
         </div>
