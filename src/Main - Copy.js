b2289df3de49d3f9d3b4c2e37cd22c0427e0aef9
Ashergreen82 <greena@rogers.com>
2023-02-16 05:46:33 -0500

    Navbar is now formatted correctly

--- src/Main - Copy.js
+++ src/Main - Copy.js
@@ -14,20 +14,20 @@ function mainNavBar() {
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse w-100" id="navbarNav">
-                    <ul className="navbar-nav d-flex flex-row w-100 justify-content-between">
-                        <li className="nav-item col">
+                    <ul className="navbar-nav d-flex flex-row w-100 justify-content-between border border-warning">
+                        <li className="nav-item col-2">
                             <a className="nav-link" href="#">Main</a>
                         </li>
                         {/* <div className="vr mx-5"></div> */}
                         <li className="nav-item col-8">
-                            <form className="d-flex flex-row justify-content-evenly m-0 p-0">
-                                <input className="form-control w-75" type="search" placeholder="Enter your twitter search here" aria-label="Search" />
-                                <button className="btn btn-outline-success" type="submit">Search</button>
+                            <form className="d-flex flex-row flex-nowrap m-0 p-0">
+                                <input className="form-control col-8" type="search" placeholder="Enter your twitter search here" aria-label="Search" />
+                                <button className="btn col-4 btn-outline-success" type="submit">Search</button>
                             </form>
                             {/* <a className="nav-link" href="#">Search</a> */}
                         </li>
                         {/* <div className="vr mx-5"></div> */}
-                        <li className="nav-item col">
+                        <li className="nav-item col-2">
                             <a className="nav-link" href="#">Random Search</a>
                         </li>
                     </ul>
