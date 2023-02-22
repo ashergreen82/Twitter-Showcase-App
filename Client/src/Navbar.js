222abd4b140dd8fb649713a84d1e7c344d66b10a
Ashergreen82 <greena@rogers.com>
2023-02-22 07:02:43 -0500

    Modified navbar.js

--- Client/src/Navbar.js
+++ Client/src/Navbar.js
@@ -29,7 +29,7 @@ function NavBar({ data, setData }) {
         //         setData(data)
         //         console.log(data)
         //     })
-        fetch('/api/search'), {
+        fetch('/api/search', {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json'
@@ -37,56 +37,73 @@ function NavBar({ data, setData }) {
             body: JSON.stringify({
                 query: searchValue
             })
-                //         .then(response => response.json())
-                //         .then(data => {
-                //             setData(data)
-                //             console.log(data)
-                //         })
-                // };
-                .then(response => response.json())
-                .then(response => {
-                    console.log(response);
-                    return response.json();
-                })
-                .then(data => {
-                    setData(data);
-                    console.log(data);
-                    // do something with the response data
-                })
-                .catch(error => {
-                    console.error(error);
-                });
-            console.log("Data Received")
-        }
-        return (
-            <>
-                <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                    <a className="navbar-brand" href="#"></a>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse w-100" id="navbarNav">
-                        <ul className="navbar-nav d-flex flex-row w-100 justify-content-between">
-                            <li className="nav-item col">
-                                <Link className="nav-link" to="/">Main</Link>
-                            </li>
-                            {/* <div className="vr mx-5"></div> */}
-                            <li className="nav-item col-8">
-                                <form className="d-flex flex-row justify-content-evenly m-0 p-0">
-                                    <input className="form-control w-75" type="search" placeholder="Enter your twitter search here" aria-label="Search" value={searchValue} onChange={handleSearchInputChange} />
-                                    <button className="btn btn-outline-success" type="submit" onClick={handleSearchButtonClick}>Search</button>
-                                </form>
-                                {/* <a className="nav-link" href="#">Search</a> */}
-                            </li>
-                            {/* <div className="vr mx-5"></div> */}
-                            <li className="nav-item col">
-                                <Link className="nav-link" to="/Randomsearch">Random Search</Link>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
-            </>
-        )
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                // do something with the response data
+            })
+            .catch(error => {
+                console.error(error);
+            });
+        // fetch('/api/search'), {
+        //     method: 'GET',
+        //     headers: {
+        //         'Content-Type': 'application/json'
+        //     },
+        //     body: JSON.stringify({
+        //         query: searchValue
+        //     })
+        //         //         .then(response => response.json())
+        //         //         .then(data => {
+        //         //             setData(data)
+        //         //             console.log(data)
+        //         //         })
+        //         // };
+        //         .then(response => response.json())
+        //         .then(response => {
+        //             console.log(response);
+        //             return response.json();
+        //         })
+        //         .then(data => {
+        //             setData(data);
+        //             console.log(data);
+        //             // do something with the response data
+        //         })
+        //         .catch(error => {
+        //             console.error(error);
+        //         });
+        console.log("Data Received")
     }
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <a className="navbar-brand" href="#"></a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse w-100" id="navbarNav">
+                    <ul className="navbar-nav d-flex flex-row w-100 justify-content-between">
+                        <li className="nav-item col">
+                            <Link className="nav-link" to="/">Main</Link>
+                        </li>
+                        {/* <div className="vr mx-5"></div> */}
+                        <li className="nav-item col-8">
+                            <form className="d-flex flex-row justify-content-evenly m-0 p-0">
+                                <input className="form-control w-75" type="search" placeholder="Enter your twitter search here" aria-label="Search" value={searchValue} onChange={handleSearchInputChange} />
+                                <button className="btn btn-outline-success" type="submit" onClick={handleSearchButtonClick}>Search</button>
+                            </form>
+                            {/* <a className="nav-link" href="#">Search</a> */}
+                        </li>
+                        {/* <div className="vr mx-5"></div> */}
+                        <li className="nav-item col">
+                            <Link className="nav-link" to="/Randomsearch">Random Search</Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </>
+    )
+}
 
-    export default NavBar
+export default NavBar
