3cc3bf24a3467ec7f61e51c062d403319ffb04ab
Ashergreen82 <greena@rogers.com>
2023-03-04 13:19:35 -0500

    More refactoring

--- Client/src/Navbar.js
+++ Client/src/Navbar.js
@@ -38,7 +38,6 @@ function NavBar({ data, setData, searchValue, setSearchValue, isSearching, setIs
     return (
         <>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                {/* <a className="navbar-brand" href="#"></a> */}
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
@@ -52,17 +51,13 @@ function NavBar({ data, setData, searchValue, setSearchValue, isSearching, setIs
                                 Main
                             </Link>
                         </li>
-                        {/* <div className="vr mx-5"></div> */}
                         <li className="nav-item col-8">
                             <form className="d-flex flex-row justify-content-evenly m-0 p-0">
                                 <input className="form-control w-75" type="search" placeholder="Enter your twitter search here" aria-label="Search" value={searchValue} onChange={handleSearchInputChange} />
                                 <button className="btn btn-outline-success" type="submit" onClick={handleSearchButtonClick}>Search</button>
                             </form>
-                            {/* <a className="nav-link" href="#">Search</a> */}
                         </li>
-                        {/* <div className="vr mx-5"></div> */}
                         <li className="nav-item col">
-                            {/* <Link className="nav-link" to="/Randomsearch">Random Search</Link> */}
                             <Link
                                 className="nav-link"
                                 to="/Randomsearch"
