74d1ab18831d64ec8fa006b0842b5cd1e12079ee
Ashergreen82 <greena@rogers.com>
2023-03-04 10:49:21 -0500

    adjusted the icons
    Got the icons properly spaced the way I wanted them.  And made changed
    the icon sized to be a 50px on both the top and bottom.

--- Client/src/components/TweetDisplay.js
+++ Client/src/components/TweetDisplay.js
@@ -8,18 +8,18 @@ function TweetDisplay({ data, setData }) {
             <h2>{data.username}</h2>
             <p>{data.full_text}</p>
             <img src={data.image} alt="No Image Available" className="img-style"></img>
-            <div className="tweet-icons container d-flex justify-content-center my-2 gx-1">
-                <div className="tweet-icons d-flex">
-                    <div className="col">
+            <div className="tweet-icons container d-flex justify-content-center">
+                <div className="tweet-icons d-flex align items-center jusity-content-center">
+                    <div className="col px-2">
                         <img src={reTweetPicture} alt="Retweet: " className="img-icon" id="retweet_icon"></img>
                     </div>
-                    <div className="col gl-5 mt-3 ">
+                    <div className="col mt-2">
                         <span className="img-value" id="retreat_Value">{data.retweet_count}</span>
                     </div>
-                    <div className="col pl-1 mt-1">
+                    <div className="col px-2 ms-5">
                         <img src={heartPicture} alt="Favourited: " className="img-icon" id="favourite_icon"></img>
                     </div>
-                    <div className="col mt-3">
+                    <div className="col mt-2">
                         <span className="img-value" id="favourite_value">{data.favorite_count}</span>
                     </div>
                 </div>
